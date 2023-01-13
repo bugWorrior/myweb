ee635433eea2f736196679b35a5b4b0a7d8dfcb6
bugWorrior <121550980+bugWorrior@users.noreply.github.com>
2023-01-13 19:01:28 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const nav = document.querySelector('nav');
-const toggleButton = document.createElement('button');
-toggleButton.innerHTML = 'Menu';
-nav.appendChild(toggleButton);
-
-toggleButton.addEventListener('click', () => {
-  nav.classList.toggle('responsive');
-});
